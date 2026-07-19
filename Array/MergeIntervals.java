ae6b4f18ca91318ec64672b5da2f280b04376438
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-07-19 13:24:21 +0530

    Add MergeIntervals class to merge overlapping intervals in an array

---
+++ Array/MergeIntervals.java
@@ -0,0 +1,36 @@
+package Array;
+import java.util.*;
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+        // Sort intervals based on starting time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
+        int[] current = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                result.add(current);
+                current = intervals[i];
+            }
+        }
+        result.add(current);
+        return result.toArray(new int[result.size()][]);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Number of intervals
+        int n = sc.nextInt();
+        int[][] intervals = new int[n][2];
+        // Read intervals
+        for (int i = 0; i < n; i++) {
+            intervals[i][0] = sc.nextInt();
+            intervals[i][1] = sc.nextInt();
+        }
+        int[][] ans = merge(intervals);
+        // Print merged intervals
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i][0] + " " + ans[i][1]);
+        }
+    }
+}
