20b033efbb8cdf7e6c81732cde68e2576fb03615
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-07-22 17:56:55 +0530

    Add ValidParanthesis class to check if parentheses are valid in a string

---
+++ String/ValidParanthesis.java
@@ -0,0 +1,29 @@
+package String;
+import java.util.*;
+public class ValidParanthesis {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack <Character>();
+        for(char c : s.toCharArray()){
+            if(c == '(')
+                stack.push(')');
+            else if(c == '{')
+                stack.push('}');
+            else if(c == '[')
+                stack.push(']');
+            else if(stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Read the parentheses string
+        String s = sc.nextLine();
+        if (isValid(s)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        sc.close();
+    }
+}
