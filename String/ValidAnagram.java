d80c6e008db9b385bf52eb7389ef5a72f67f133d
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-07-24 14:00:53 +0530

    Add ValidAnagram class to check if two strings are anagrams

---
+++ String/ValidAnagram.java
@@ -0,0 +1,29 @@
+package String;
+import java.util.*;
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        boolean result = isAnagram(s, t);
+        System.out.println(result);
+    }
+}
