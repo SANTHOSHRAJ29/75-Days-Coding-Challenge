c23954549d81cb242df0e4ed8292da3716842f7e
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-08-08 18:56:39 +0530

    add PrintAllTheDuplicateCharactersInString class to identify and print duplicate characters in a string

---
+++ String/PrintAllTheDuplicateCharactersInString.java
@@ -0,0 +1,20 @@
+package String;
+import java.util.*;
+public class PrintAllTheDuplicateCharactersInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            count[ch]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (count[ch] > 1) {
+                System.out.println("['" + ch + "', " + count[ch] + "]");
+                count[ch] = 0;
+            }
+        }
+    }
+}
