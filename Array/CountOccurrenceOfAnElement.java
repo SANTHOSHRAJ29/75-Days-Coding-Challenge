1f9984a17dc6a86883f45362e781a950db377ce8
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-08-18 17:31:14 +0530

    add CountOccurrenceOfAnElement class to count occurrences of a target element in an array

---
+++ Array/CountOccurrenceOfAnElement.java
@@ -0,0 +1,20 @@
+package Array;
+import java.util.*;
+public class CountOccurrenceOfAnElement {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        int target = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            if(arr[i] == target){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
