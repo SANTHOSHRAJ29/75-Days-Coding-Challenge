62b692e137b8f532e3bc2fccb1225d851ee63b34
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-09-09 20:03:47 +0530

    add FindTheLengthOfAStringWithoutUsingALength class to calculate the length of a string without using the length method

---
+++ String/FindTheLengthOfAStringWithoutUsingALength.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.*;
+public class FindTheLengthOfAStringWithoutUsingALength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for (char character : str.toCharArray()) {
+            count++;
+        }
+        System.out.println("Length = " + count);
+    }
+}
