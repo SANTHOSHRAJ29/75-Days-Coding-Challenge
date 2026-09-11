a3b5f00634511e7f630fd6a22e1402201c6ba7f7
Santhosh Raj S <santhoshraj565758@gmail.com>
2026-09-11 18:12:31 +0530

    add ConverUpperCaseToLowerCase class to convert uppercase letters in a string to lowercase

---
+++ String/ConverUpperCaseToLowerCase.java
@@ -0,0 +1,18 @@
+package String;
+import java.util.*;
+public class ConverUpperCaseToLowerCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch + 32);
+            }
+            result = result + ch;
+        }
+        System.out.println("Lowercase = " + result);
+    }
+}
